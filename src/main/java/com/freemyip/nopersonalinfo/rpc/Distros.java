2331366d116ef8452290bb6da371ede56e6307e9
151044 <151044@std.stpaulscollege.edu.hk>
2021-03-21 19:53:19 +0800

    Preliminary work on distro detection

--- src/main/java/com/freemyip/nopersonalinfo/rpc/Distros.java
+++ src/main/java/com/freemyip/nopersonalinfo/rpc/Distros.java
@@ -1,10 +1,46 @@
 package com.freemyip.nopersonalinfo.rpc;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
 public class Distros {
     private Distros(){
         throw new AssertionError("Distros not instantiable!");
     }
+    private static String distroPath = "unknown";
+    private static String distroName = "unknown";
     static{
+        //First read /etc/os-release (systemd), then read /etc/issue
+        String distro = "unknown";
+        Path lsbRelease = Path.of("/etc/os-release");
+        try {
+            if (lsbRelease.toFile().exists()) {
+                Optional<String> optDistro = Files.readAllLines(Path.of("")).stream().filter(s -> s.startsWith("PRETTY_NAME")).map(s -> s.strip().split("=")[1].replace("\"",""))
+                        .findFirst();
+                if(optDistro.isPresent()){
+                    distro = optDistro.get();
+                    String lookup = distro.toLowerCase();
+                }
+            } else {
+                Path issue = Path.of("/etc/issue");
+                if (issue.toFile().exists()) {
 
+                }
+            }
+        }catch(IOException ioe){
+            if(Main.getDebugState()){
+                ioe.printStackTrace();
+            }
+        }
+    }
+
+    public static String getDistroName() {
+        return distroName;
+    }
+
+    public static String getDistroPath() {
+        return distroPath;
     }
 }
